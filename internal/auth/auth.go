9926c62a0ea969caf3200678fdad15357819eb19
pearlMat <pearllyt@gmail.com>
2023-12-07 14:49:37 +0100

    created auth file

---
+++ internal/auth/auth.go
@@ -0,0 +1,24 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+
+func GetApiKey(headers http.Header)(string, error){
+	val := headers.Get("Authorization")
+	if val == ""{
+		return "", errors.New("no token found")
+	}
+	vals := strings.Split(val, " ")
+	if len(vals) != 2{
+		return "", errors.New("header not valid")
+	}
+
+	if vals[0] != "ApiKey"{
+		return "", errors.New("malformed first part")
+	}
+	return vals[1], nil
+}
